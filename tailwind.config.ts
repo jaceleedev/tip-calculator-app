bd3efade7d6d794bf133b04a6169c6667b3cbe85
jaceleedev <jaceleedev@gmail.com>
2024-08-19 20:13:04 +0900

    feat: :wrench: implement colors and typography from Figma design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,74 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'very-light-grayish-cyan': '#F3F9FA',
+        'light-grayish-cyan': '#C5E4E7',
+        'light-strong-cyan': '#9FE8DF',
+        'strong-cyan': '#26C2AE',
+        'grayish-cyan': '#7F9D9F',
+        'medium-dark-grayish-cyan': '#547878',
+        'dark-grayish-cyan': '#5E7A7D',
+        'strong-orange-red': '#E17052',
+        'very-dark-grayish-cyan': '#0D686D',
+        'very-dark-cyan': '#00474B',
+      },
+      fontFamily: {
+        spaceMono: ['var(--font-space-mono)'],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const typographyUtilities = {
+        '.text-primary': {
+          fontFamily: 'var(--font-space-mono)',
+          fontSize: '24px',
+          fontStyle: 'normal',
+          fontWeight: '700',
+          lineHeight: '1.5',
+        },
+        '.text-secondary': {
+          fontFamily: 'var(--font-space-mono)',
+          fontSize: '20px',
+          fontStyle: 'normal',
+          fontWeight: '700',
+          lineHeight: '1.5',
+        },
+        '.text-label': {
+          fontFamily: 'var(--font-space-mono)',
+          fontSize: '16px',
+          fontStyle: 'normal',
+          fontWeight: '700',
+          lineHeight: '1.5',
+        },
+        '.text-caption': {
+          fontFamily: 'var(--font-space-mono)',
+          fontSize: '13px',
+          fontStyle: 'normal',
+          fontWeight: '700',
+          lineHeight: '19px',
+        },
+        '.text-display': {
+          fontFamily: 'var(--font-space-mono)',
+          fontSize: '32px',
+          fontStyle: 'normal',
+          fontWeight: '700',
+          lineHeight: '47px',
+          letterSpacing: '-0.667px',
+        },
+      };
+
+      addUtilities(typographyUtilities);
+    }),
+  ],
 };
 export default config;
