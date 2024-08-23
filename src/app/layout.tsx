a28fba0e1d4815aec28bfb6520adfabb30272eca
jaceleedev <jaceleedev@gmail.com>
2024-08-23 16:37:57 +0900

    refactor: :recycle: change div to header for Logo component container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -101,9 +101,9 @@ export default function RootLayout({
       <body
         className={`flex flex-col items-center w-screen pt-[50px] bg-light-grayish-cyan ${spaceMono.variable} lg:pt-[163px]`}
       >
-        <div className="flex justify-center mb-[40.86px] lg:mb-[87.86px]">
+        <header className="flex justify-center mb-[40.86px] lg:mb-[87.86px]">
           <Logo />
-        </div>
+        </header>
         {children}
       </body>
     </html>
